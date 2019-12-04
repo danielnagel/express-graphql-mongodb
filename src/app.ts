8c0f0bbab0e250e2e01fe991faf417f368a58904
Daniel Nagel <daniel.nagel@propersonal.de>
2019-12-04 11:42:51 +0100

    Removed config.ts from git, added dotenv support

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,16 @@ import cors from 'cors';
 import morgan from 'morgan';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-
-import config from './config';
 import messages from './db/messages';
 import users from './db/users';
 import graphqlHTTP from 'express-graphql';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const PORT = 3001;
+const SECRET: string = (process.env.SECRET as string) || 'ilovesaltythings';
 const app = express();
 
-app.set('port', process.env.PORT || PORT);
+app.set('port', process.env.PORT || 3001);
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
@@ -45,7 +45,7 @@ app.post('/register', (req, res) => {
     users
         .create(req.body)
         .then(user => {
-            const token = jwt.sign({ id: user._id }, config.secret, {
+            const token = jwt.sign({ id: user._id }, SECRET, {
                 expiresIn: 86400, // 24h
             });
             res.json({ auth: true, token: token, user: user });
@@ -62,7 +62,7 @@ app.post('/login', (req, res) => {
             if (!user) return res.status(404).send('No user found.');
             const passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
             if (!passwordIsValid) return res.status(401).json({ auth: false, token: null });
-            const token = jwt.sign({ id: user._id }, config.secret, {
+            const token = jwt.sign({ id: user._id }, SECRET, {
                 expiresIn: 86400, // 24h
             });
             res.json({ auth: true, token: token, user: user });
